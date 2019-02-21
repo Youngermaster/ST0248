06661789958be3a2cb06b2588a83b24b0031cb0d
Youngermaster <jmyoungh@eafit.edu.co>
2019-02-21 17:46:55 -0500

    I have commented the script 'taller3.ino'

--- Taller 3/code/taller3.ino
+++ Taller 3/code/taller3.ino
@@ -1,90 +1,132 @@
+/*------------------------------------------------
+* Electrónica Digital                            *
+* Taller 3                                       *
+*                                                *
+* Description:                                   *
+*                                                *
+*                                                *
+* Members:                                       *
+*          - Camila Barona Cabrera               *
+*          - Maria Antonia Rincón                *
+*          - Valeria Suarez                      *
+*          - Sofia Uribe                         *
+*          - Juan Manuel Young Hoyos             *
+*                                                *
+* Teacher: David Velásquez                       *
+*                                                *
+* Created 25/02/2019  (DD/MM/YYYY)               *
+*                                                *
+-------------------------------------------------*/
+
+// These are the elements that will be conect to the arduino's pins
+// You can modify they.
+#define RED_LED 7        // This allude to the red led.
+#define YELLOW_LED 6     // This allude to the yellow led.
+#define BLUE_LED 5       // This allude to the blue led.
+#define GREEN_LED 4      // This allude to the green led.
+#define POTENTIOMETER 0 // This allude to the potentiometer.
+
+float potenMeterV = 0; // It saves the potentiometer's value.
+
 /**
-* Electrónica Digital
-* Taller 3
-* Members:
-*          - Camila Barona Cabrera
-*          - Maria Antonia Rincón
-*          - Valeria Suarez
-*          - Sofia Uribe
-*          - Juan Manuel Young Hoyos
-*
-* Teacher: David Velásquez
-*
-* Description: 
-*/
-
-#define REDLED 7
-#define YELLOWLED 6
-#define BLUELED 5
-#define GREENLED 4
-#define POTENTIOMETER 0
-
-// It saves the potentiometer's value.
-float potenMeterV = 0; 
-
+ * parameters: ledNumber -> int.
+ * returns: void.
+ * Depending of the parameter the method choose which color turn on.
+ */
 void turnOnLed(int ledNumber) 
 {
   switch(ledNumber) 
   {
-  	case 1:
+  	case 1: // Case 1: turn on the green LED.
       turnOffLeds();
-      digitalWrite(GREENLED, HIGH);
+      digitalWrite(GREEN_LED, HIGH);
       break;
-    case 2:
+    case 2: // Case 2: turn on the blue LED.
       turnOffLeds();
-      digitalWrite(BLUELED, HIGH);
+      digitalWrite(BLUE_LED, HIGH);
       break;
-    case 3:
+    case 3: // Case 3: turn on the yellow LED.
       turnOffLeds();
-      digitalWrite(YELLOWLED, HIGH);
+      digitalWrite(YELLOW_LED, HIGH);
       break;
-    case 4:
+    case 4: // Case 4: turn on the red LED.
       turnOffLeds();
-      digitalWrite(REDLED, HIGH);
-    default:
+      digitalWrite(RED_LED, HIGH);
+    default: // default: do nothing.
       break;
   }
 }
 
+/**
+ * parameters: none.
+ * returns: void.
+ * Turn off all the LEDs.
+ */
 void turnOffLeds()
 {
-  digitalWrite(REDLED, LOW);
-  digitalWrite(BLUELED, LOW);
-  digitalWrite(GREENLED, LOW);
-  digitalWrite(YELLOWLED, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
 }
 
+/**
+ * parameters: potentMeter -> float.
+ * returns: void.
+ * Prints the voltage with the potentMeter value.
+ */
+void printsVoltage(float potentMeter)
+{
+  Serial.print("Voltage value: ");
+  Serial.println(potentMeter * 5.0 / 1023.0);
+}
+
+/**
+ * parameters: none.
+ * returns: void.
+ * This is core of the program and this have the conditionals to determine which
+ * LED it should turn on.
+ */
 void core()
 {
-  potenMeterV = analogRead(POTENTIOMETER);
-  Serial.print("Voltage value: ");
-  Serial.println(potenMeterV * 5.0 / 1023.0);
-  
-  if (potenMeterV <= 1023 && potenMeterV >= 767)
+  potenMeterV = analogRead(POTENTIOMETER); // Reads the 'value' of the potentiometer
+  printsVoltage(potenMeterV);              // Prints the potentiometer's voltage
+
+  if (potenMeterV <= 1023 && potenMeterV >= 767)      // Turn on the green LED
   {
     turnOnLed(1);
-  } else if (potenMeterV < 767 && potenMeterV >= 511)
+  } else if (potenMeterV < 767 && potenMeterV >= 511) // Turn on the blue LED
   { 
     turnOnLed(2);
-  } else if (potenMeterV < 511 && potenMeterV >= 256)
+  } else if (potenMeterV < 511 && potenMeterV >= 256) // Turn on the yellow LED
   { 
     turnOnLed(3);
-  } else if (potenMeterV < 256 && potenMeterV > 0)
+  } else if (potenMeterV < 256 && potenMeterV > 0)    // Turn on the red LED
   { 
     turnOnLed(4);
   }
-  
 }
 
+/**
+ * parameters: none.
+ * returns: void.
+ * Runs once and setting up your Arduino for the rest of the program.
+ */
 void setup()
 {
-  pinMode(REDLED, OUTPUT);
-  pinMode(YELLOWLED, OUTPUT);
-  pinMode(BLUELED, OUTPUT);
-  pinMode(GREENLED, OUTPUT);
-  Serial.begin(9600);
+  pinMode(RED_LED, OUTPUT);      // Defines the led red as a output
+  pinMode(YELLOW_LED, OUTPUT);   // Defines the led yellow as a output
+  pinMode(BLUE_LED, OUTPUT);     // Defines the led blue as a output
+  pinMode(GREEN_LED, OUTPUT);    // Defines the led green as a output
+  // Sets the data rate in bits per second (baud) for serial data transmission
+  Serial.begin(9600);            
 }
 
+/**
+ * parameters: none.
+ * returns: void.
+ * Execute the core function.
+ */
 void loop()
 {
   core();
